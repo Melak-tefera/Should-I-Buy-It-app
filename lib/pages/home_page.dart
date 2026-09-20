0f0788379b8314b4cdd7d4eb3338563df138059b
Melak-tefera <tmeleak@gmail.com>
2026-09-21 00:29:05 +0100

    working withmain page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,13 +20,50 @@ class _HomePageState extends State<HomePage> {
             children: [
               Row(
                 children: [
-
+                  Text(
+                    "Should I Buy It?",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 40,
+                    ),
+                    
+                    ),
+                  Spacer(),
+                  IconButton(
+                    onPressed: (){}, 
+                    icon: Icon(
+                      Icons.timelapse_outlined,
+                      size: 30,
+                      color: Colors.white,
+                      
+                      )
+                    )
                 ],
               ),
-              Text(""),
-              SizedBox(),
+              Text(
+                "Search for something you want. we'll turn its price into the time you'd need to work for it.",
+                style: TextStyle(
+                  color: Colors.white,
+                  
+                ),
+                ),
+              SizedBox(height: 40,),
+
+              Align(
+                alignment: AlignmentGeometry.bottomLeft,
+                child: Text(
+                  "What do want to buy?",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  ),
+              ),
+                SizedBox(height: 10,),
               // there is a searchbar
-              TextField(),
+              SearchBar(
+                backgroundColor: WidgetStateProperty.resolveWith((states) {return const Color(0xFF1E1E1E); }),
+              ),
               Text("Your Pick"),
               // green transparant container
               Container(
