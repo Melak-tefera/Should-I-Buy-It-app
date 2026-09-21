f61c3956176618693a4948ec03b33613cd5d51ac
Melak-tefera <tmeleak@gmail.com>
2026-09-21 09:43:30 +0100

    working on home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,20 +15,28 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(15.0),
           child: Column(
             children: [
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Should I Buy It?",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 40,
-                    ),
-                    
-                    ),
+                  SizedBox(
+                    width: 200,
+                    height: 100,
+                    child: Text(
+                      "Should I \nBuy It?",
+                      maxLines: 2,
+                      
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        height: 1
+                      ),
+                      
+                      ),
+                  ),
                   Spacer(),
                   IconButton(
                     onPressed: (){}, 
@@ -41,13 +49,16 @@ class _HomePageState extends State<HomePage> {
                     )
                 ],
               ),
-              Text(
-                "Search for something you want. we'll turn its price into the time you'd need to work for it.",
-                style: TextStyle(
-                  color: Colors.white,
-                  
-                ),
-                ),
+              Align(
+                alignment: AlignmentGeometry.bottomLeft,
+                child: Text(
+                  "Search for something you want. we'll turn its \nprice into the time you'd need to work for it.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    
+                  ),
+                  ),
+              ),
               SizedBox(height: 40,),
 
               Align(
@@ -56,15 +67,56 @@ class _HomePageState extends State<HomePage> {
                   "What do want to buy?",
                   style: TextStyle(
                     color: Colors.white,
+                    fontSize: 20
                   ),
                   ),
               ),
                 SizedBox(height: 10,),
               // there is a searchbar
               SearchBar(
+                leading: Icon(Icons.search, color: Colors.white, size: 26,),
+                hintText: "What to buy?", 
+                
                 backgroundColor: WidgetStateProperty.resolveWith((states) {return const Color(0xFF1E1E1E); }),
+                hintStyle: WidgetStateProperty.all(
+                const TextStyle(
+                  color: Colors.white54, // lighter hint text
+                  fontSize: 16,
+                ),
               ),
-              Text("Your Pick"),
+              textStyle: WidgetStateProperty.all(
+                const TextStyle(
+                  color: Colors.white, // typed text color
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                ),
+  ),
+              ),
+              SizedBox(height: 10,),
+              Align(
+                alignment: AlignmentGeometry.bottomLeft,
+                child: Text(
+                  "Your Pick",
+                  style:TextStyle(
+                      color: Colors.white,
+                      fontSize: 17
+                    ),
+                  ),
+              ),
+              SizedBox(height: 10,),
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Container(
+                  height: 70,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(212, 47, 47, 47),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+
+
               // green transparant container
               Container(
               
